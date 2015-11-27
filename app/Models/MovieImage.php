32844146bf339f69bdd2740627f84b695f289c88
Dominik Jungowski <Dominik@Jungowski.de>
2015-11-28 00:25:28 +0100

    Movie image classes + score11 config

---
+++ app/Models/MovieImage.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Models;
+
+class MovieImage
+{
+	private $_movieId;
+
+	private $_hasImage;
+
+	private $_movieImagePath;
+
+	public function __construct($movieId, $hasImage = 'y')
+	{
+		$this->_movieId = $movieId;
+		$this->_hasImage = $hasImage;
+		$host = config('app.host');
+		$path = config('app.movieimgpath');
+		$this->_movieImagePath = $host . '/' . $path;
+	}
+
+	public function getLink()
+	{
+		if ($this->_hasImage == 'n') {
+			$link = config('app.movie-logo');
+		} else {
+			$lastTwo = substr($this->_movieId, -2, 2);
+			if (strlen($lastTwo) == 1) {
+				$lastTwo = "0" . $lastTwo;
+			}
+			// %s statt %d fuer den zweiten paraemter verwendet, da sonst aus 05 5 wird und dann stimmt der Bildlink nicht mehr
+			$link = sprintf('%s/%s/%s', $this->_movieImagePath, $lastTwo, $this->_movieId);
+		}
+		return $link;
+	}
+}
---
+++ tests/app/Models/MovieImageTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\MovieImage;
+
+class MovieImageTest extends TestCase
+{
+	public function testGetLinkWithoutImage()
+	{
+		$image = new MovieImage(28786, 'n');
+		$expected = "img/logo-movie.png";
+		$actual = $image->getLink();
+		self::assertSame($expected, $actual);
+	}
+
+	public function testGetLinkWithImage()
+	{
+		$image = new MovieImage(28786);
+		$expected = "http://www.score11.de/p/86/28786";
+		$actual = $image->getLink();
+		self::assertSame($expected, $actual);
+	}
+
+	public function testGetLinkWith1DigitFolder()
+	{
+		$image = new MovieImage(5);
+		$expected = "http://www.score11.de/p/05/5";
+		$actual = $image->getLink();
+		self::assertSame($expected, $actual);
+	}
+}
