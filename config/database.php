c70e54bbd979287163cd22a06532b6d38a315711
Alexander Ermer <github@allanon-coding.de>
2015-11-29 01:51:51 +0100

    testing database

--- config/database.php
+++ config/database.php
@@ -56,6 +56,17 @@ return [
             'prefix'    => '',
             'strict'    => false,
         ],
+        'mysql_testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('TESTING_DB_HOST', 'localhost'),
+            'database'  => env('TESTING_DB_DATABASE', 'score11_testing'),
+            'username'  => env('TESTING_DB_USERNAME', 'root'),
+            'password'  => env('TESTING_DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
     ],
 
     /*
