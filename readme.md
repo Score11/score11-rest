9a2479b0ee7b8afc81d4375532f988c42b8d7bfb
Alexander Ermer <github@allanon-coding.de>
2015-11-29 12:27:43 +0100

    moviestart json full tests + updated readme for installation

--- readme.md
+++ readme.md
@@ -12,6 +12,12 @@
 3. Copy environment-configuration `copy .env.example .env`
 4. Optional: customize your local config
 5. Run `./setup.sh`
+6. Setup mysql scheme for your local app
+    - Run `mysql -e "create database score11;" -uroot`
+    - import dump to `score11` scheme or run `php artisan migrate` on laravel root directory
+7. Setup mysql scheme for unit tests
+    - Run `mysql -e "create database score11_testing;" -uroot`
+    - Run `php artisan --database=mysql_testing migrate` on laravel root directory
 
 ## Running
 ### Start server
--- tests/integration/MovieStartJsonTest.php
+++ tests/integration/MovieStartJsonTest.php
@@ -1,31 +1,59 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Movie;
+use App\MovieStart;
+use App\Title;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class MovieStartJsonTest extends TestCase
 {
-    use DatabaseTransactions;
-    use DatabaseMigrations;
+    use DatabaseTransactions; // do not store anything to database
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testJsonContainsMovie()
     {
-        $movie = factory(\App\Movie::class)->create();
-        $movie->titles()->save(factory(\App\Title::class)->make());
-        $movie->moviestart()->save(factory(\App\MovieStart::class)->make());
+        // insert a film with title and moviestart
+        $movie = factory(Movie::class)->create();
+        $movie->titles()->save(factory(Title::class)->make());
+        $movie->moviestart()->save(factory(MovieStart::class)->make());
 
+        // get moviestarts json and check if movie is in it
         $this->get('/api/moviestart')
             ->seeJson([
                 'id' => $movie->id,
                 'startdate' => $movie->moviestart()->first()->date,
                 'title' => $movie->titles()->first()->title
+            ]);
+    }
+
+    public function testJsonContainsMovieExactMatch()
+    {
+        // insert a film with title and moviestart
+        $movie = factory(Movie::class)->create(['image' => 'n']);
+        $movie->titles()->save(factory(Title::class)->make());
+        $movie->moviestart()->save(factory(MovieStart::class)->make());
+
+        // get moviestarts json and check if movie is in it
+        $this->get('/api/moviestart')
+            ->seeJsonEquals([
+                'data' => [
+                    [
+                        'id' => $movie->id,
+                        'startdate' => $movie->moviestart()->first()->date,
+                        'image' => 'http://www.score11.de/2012/img/logo-movie.png',
+                        'titles' => [
+                            'data' => [
+                                [
+                                    'title' => $movie->titles()->first()->title,
+                                    'version' => $movie->titles()->first()->version,
+                                    'year' => $movie->titles()->first()->year
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'meta' => [
+                    'total' => 1
+                ]
             ]);
     }
 }
